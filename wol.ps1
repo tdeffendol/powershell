73cfc93758e13ce7079a065ebcecb6dd003a5142
PxWebDev <tdeffendol@gmail.com>
2024-03-17 12:21:55 -0600

    Example WOL script

---
+++ wol.ps1
@@ -0,0 +1,8 @@
+$Mac = "E0:D5:5e:A4:E0:56"
+#macaddress of machine you want to turn on
+$MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+$UdpClient.Close()
